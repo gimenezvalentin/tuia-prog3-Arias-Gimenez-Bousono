66a13126697ac11e95bb654a306a80ac79163e41
unknown <federicomarias@gmail.com>
2025-10-19 19:37:55 -0300

    Agregando comentarios para defensa algoritmos de busqueda

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -19,14 +19,18 @@ class AStarSearch:
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
 
         # Initialize reached with the initial state
+        #'Reached' almacena el menor costo real (g(n)) conocido para llegar a cada estado
         reached = {}
         reached[root.state] = root.cost
 
         # Initialize frontier with the root node
         frontier = PriorityQueueFrontier()
+        #f(n) = g(n) + h(n) 
+        #g(n) es root.cost y h(n) es grid.hmanhattan(root).
         frontier.add(root, root.cost + grid.hmanhattan(root))
         
         while not frontier.is_empty():
+            #El nodo extraido es siempre el que tiene el menor costo TOTAL ESTIMADO (f(n))
             node = frontier.pop()
 
             if grid.objective_test(node.state):
@@ -35,12 +39,17 @@ class AStarSearch:
             for action in grid.actions(node.state):
 
                 state = grid.result(node.state, action)
+                #g(n): Costo real acumulado para llegar al sucesor
                 cos = node.cost + grid.individual_cost(node.state, action)
 
-
+                #Solo procesamos si encontramos el estado por primera vez O si el nuevo
+                #camino tiene un costo REAL (g(n)) menor que el previamente registrado
                 if state not in reached or cos < reached[state]:
                     new_node = Node("", state, cos, node, action)
+                    #Actualizamos el menor costo real conocido (g(n))
                     reached[state] = cos
+                    #Añadimos el nodo a la frontera usando la 
+                    # función f(n) como su prioridad
                     frontier.add(new_node, new_node.cost + grid.hmanhattan(new_node))
 
 
--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -22,23 +22,30 @@ class GreedyBestFirstSearch:
         reached[root.state] = root.cost
         
         frontier = PriorityQueueFrontier()
+        #la prioridad del nodo en la frontera es SOLO
+        # la función heurística (h(n)), ignorando el costo real acumulado (g(n))!!!
         frontier.add(root, grid.hmanhattan(root))
-        print(grid.h(root))
         
 
         while not frontier.is_empty():
+            #El nodo extraido es el que parece estar mas cerca del objetivo,
+            #segun la estimación heurística 
             node = frontier.pop()
             if grid.objective_test(node.state):
                 return Solution(node, reached)
             
             for action in grid.actions(node.state):
                 state = grid.result(node.state, action)
+                #'cos' (costo acumulado) se calcula pero SOLO se usa para actualizar 'reached'!
+                #NO! se usa para ordenar la frontera
                 cos = node.cost + grid.individual_cost(node.state, action)
-
+                #A diferencia de A*, la condición de reemplazo
+                #aca es solo para evitar ciclos infinitos, no para garantizar el menor costo
                 if state not in reached or cos < reached[state]:
                     new_node = Node("", state, cos, node, action)
                     reached[state] = cos
-                    frontier.add(new_node, grid.h(new_node))
-                    print(grid.hmanhattan(new_node))
+                    #Añadimos el nuevo nodo a la frontera
+                    #Su prioridad es grid.h(new_node), que es la función heurística (h(n))
+                    frontier.add(new_node, grid.hmanhattan(new_node))
                     
         return NoSolution(reached)
